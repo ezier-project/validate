ef62b3eb1e45fc88d5fabbc7741194d5394094e4
Shadofer <shadowrlrs@gmail.com>
2022-04-25 17:58:36 +0300

    Add validateString tests.

--- src/test/testMain.test.ts
+++ src/test/testMain.test.ts
@@ -1,3 +1,44 @@
-it("shouldn't work", done => {
-    done('Unimplemented');
+import { EzierValidatorError, validateString } from '../main/index';
+import { v4 } from 'uuid';
+
+let hasErrored = false;
+
+function checkError(result: undefined | EzierValidatorError, done: Mocha.Done): void {
+    if(result) {
+        hasErrored = true;
+        done(result.message);
+    }
+}
+
+function validateExactLength(done: Mocha.Done): void {
+    checkError(validateString('astring', {
+        length: 7
+    }), done);
+}
+
+function validateMinLength(done: Mocha.Done): void {
+    checkError(validateString('strmin', {
+        minLength: 6
+    }), done);
+}
+
+function validateMaxLength(done: Mocha.Done): void {
+    checkError(validateString('strmax', {
+        maxLength: 6
+    }), done);
+}
+
+function validateRegex(done: Mocha.Done): void {
+    checkError(validateString(v4(), {
+        regex: /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+    }), done);
+}
+
+it('string validation', done => {
+    validateExactLength(done);
+    validateMinLength(done);
+    validateMaxLength(done);
+    validateRegex(done);
+
+    !hasErrored && done();
 });
