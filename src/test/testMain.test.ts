295d4083b3dd7a021d5f058dbf6e0ea2839e3283
Shadofer <shadowrlrs@gmail.com>
2022-04-25 18:02:29 +0300

    Format code.

--- src/test/testMain.test.ts
+++ src/test/testMain.test.ts
@@ -3,35 +3,50 @@ import { v4 } from 'uuid';
 
 let hasErrored = false;
 
-function checkError(result: undefined | EzierValidatorError, done: Mocha.Done): void {
-    if(result) {
+function checkError(
+    result: undefined | EzierValidatorError,
+    done: Mocha.Done
+): void {
+    if (result) {
         hasErrored = true;
         done(result.message);
     }
 }
 
 function validateExactLength(done: Mocha.Done): void {
-    checkError(validateString('astring', {
-        length: 7
-    }), done);
+    checkError(
+        validateString('astring', {
+            length: 7,
+        }),
+        done
+    );
 }
 
 function validateMinLength(done: Mocha.Done): void {
-    checkError(validateString('strmin', {
-        minLength: 6
-    }), done);
+    checkError(
+        validateString('strmin', {
+            minLength: 6,
+        }),
+        done
+    );
 }
 
 function validateMaxLength(done: Mocha.Done): void {
-    checkError(validateString('strmax', {
-        maxLength: 6
-    }), done);
+    checkError(
+        validateString('strmax', {
+            maxLength: 6,
+        }),
+        done
+    );
 }
 
 function validateRegex(done: Mocha.Done): void {
-    checkError(validateString(v4(), {
-        regex: /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
-    }), done);
+    checkError(
+        validateString(v4(), {
+            regex: /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
+        }),
+        done
+    );
 }
 
 it('string validation', done => {
