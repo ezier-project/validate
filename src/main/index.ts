1e85601ac0491facdaf6ea06353447740c257177
Shadofer <shadowrlrs@gmail.com>
2022-04-27 18:17:07 +0300

    Add more type options.

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,13 +9,54 @@ export interface EzierValidatorSchema {
 }
 
 // Strings
-type EzierValidatorStringType = 'email';
+type EzierValidatorStringType =
+    | 'ascii'
+    | 'email'
+    | 'date'
+    | 'IP'
+    | 'IPV6'
+    | 'phone_number'
+    | 'url'
+    | 'uuid'
+    | 'MAC'
+    | 'emoji'
+    | 'PORT';
 
 const EzierValidatorStringTypeRegex: {
     [Type in EzierValidatorStringType]: RegExp;
 } = {
+    // https://ihateregex.io/expr/ascii
+    ascii: /[ -~]/,
+
     // https://ihateregex.io/expr/email
     email: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+
+    // https://ihateregex.io/expr/date
+    date: /(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})/,
+
+    // https://ihateregex.io/expr/ip
+    IP: /(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/,
+
+    // https://ihateregex.io/expr/ipv6
+    IPV6: /(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/,
+
+    // https://ihateregex.io/expr/phone
+    phone_number: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
+
+    // https://ihateregex.io/expr/url
+    url: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/,
+
+    // https://ihateregex.io/expr/uuid
+    uuid: /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/,
+
+    // https://ihateregex.io/expr/mac-address
+    MAC: /^[a-fA-F0-9]{2}(:[a-fA-F0-9]{2}){5}$/,
+
+    // https://ihateregex.io/expr/emoji
+    emoji: /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/,
+
+    // https://ihateregex.io/expr/port
+    PORT: /^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$/,
 };
 
 export interface EzierValidatorStringSchema extends EzierValidatorSchema {
